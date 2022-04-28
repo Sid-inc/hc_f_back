a1eed958cd6dbe343f36cc0c58e65e2a05d98c28
Sid-inc <sid-sked@yandex.ru>
2022-04-28 22:14:03 +0300

    Add iphone ping function

---
+++ dbSettings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: 'localhost',
+  user: 'hc_user',
+  database: 'hc',
+  password: 'ZQt4LeNBHibpy0N8'
+};
---
+++ dbquerys/getDeviceStatus.js
@@ -0,0 +1,9 @@
+const dbquery = require('../dbConnection');
+
+
+async function getDeviceStatus(id) {
+  let deviceStatus = await dbquery('SELECT `status` FROM `network_devices` WHERE `id`=' + id);
+  return deviceStatus;
+}
+
+module.exports = getDeviceStatus;
