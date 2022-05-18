6fc8206d97109f1ad7b00fc3e79180f1af5831a8
Sid-inc <sid-sked@yandex.ru>
2022-05-18 08:28:55 +0300

    Collected ping log

--- pinglogger.js
+++ pinglogger.js
@@ -5,21 +5,22 @@ const CronJob = require('cron').CronJob;
 
 let state = '0';
 
-let pinger = new CronJob('* * * * *', function() {
+let pinger = new CronJob('*/2 * * * * *', async function() {
   let d = new Date();
 
   try {
-    const { stdout, stderr } = exec(`ping -с 1 ${iphone.ip}`);
+    const { stdout, stderr } = await exec(`ping -c 1 192.168.1.10`);
     if (stdout.indexOf(' 0 received') === -1) {
       state = '1';
+	fs.appendFileSync("pinglog.txt", `1;${d.toString()}\n`);
     } else {
       state = '0';
+	fs.appendFileSync("pinglog.txt", `0;${d.toString()}\n`);
     }
   } catch (err) {
     state = '0';
+	fs.appendFileSync("pinglog.txt", `0;${d.toString()}\n`);
   }
-  
-  fs.appendFileSync("pinglog.txt", `${state};${d.toString()}\n`);
-}, null, true, '');
 
-pinger();
+//  fs.appendFileSync("pinglog.txt", `${state};${d.toString()}\n`);
+}, null, true, '');
